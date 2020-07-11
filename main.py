b0ad821ee2bd1be61855052f9b49e966736d8323
HallosF1 <60935451+HallosF1@users.noreply.github.com>
2020-07-11 23:35:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,22 +5,20 @@ while True:
     print("pls choose a option\n 1 : sum \n 2 : sub \n 3 : multiply \n 4 : divide")
 
     options = input("What is your choice ?")
-    choosen = list(map(lambda nums: int(input('enter a number : ')), range(1,3)))
-    num1 = choosen[0]
-    num2 = choosen[1]
-
+    hm_numbers = int(input("How many numbers will you use ?"))
+    choosen = list(map(lambda nums: int(input('enter a number : ')), range(1, hm_numbers + 1)))
 
     if options == '1':
-        print(calculator.sum(num1, num2))
+        print(calculator.sum(choosen))
         break
     elif options == '2':
-        print(calculator.sub(num1, num2))
+        print(calculator.sub(choosen))
         break
     elif options == '3':
-        print(calculator.multiply(num1, num2))
+        print(calculator.multiply(choosen))
         break
     elif options == '4':
-        print(calculator.divide(num1, num2))
+        print(calculator.divide(choosen))
         break
 
     else:
